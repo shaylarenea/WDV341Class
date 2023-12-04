12dcfcb97018fadcf417ba712e37525d737a33fc
Shaylarenea <shayla.williamsr@gmail.com>
2023-12-04 02:55:03 -0600

    Add git.php

---
+++ git.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>WDV 341 Intro PHP</h1>
+<h3>1-2 Download Git Client & Create a Repository account</h3><br>
+<h4>Terms Defined:</h4>
+<p><b>Version Control Software:</b>is software that is used for tracking and management changes to software code</p>is software that is used for tracking and management changes to software code.
+<p><b>Add:</b> a command ($git add filename) adds new or changed files in your Github Repository. It is an important command.</p>
+<p><b>Commit:</b>creates something like a snapshot of your respository.  The command is similar to saving a respository and should be made often. Commit demand creates a history timeline of a specific respiratory. An example of using this command: git commit -ataskexample.</p>
+<p><b>Push:</b>a command that is used to upload local respository to a remote repository. Example: git push</p>
+<p><b>Pull:</b>a command trhat is used to download content from a remote respository and instantly update the local respository to match the content.</p>
+<p><b>Clone:</b>a command used to get a duplicate on an existing respository.</p>
+</body>
+</html>
