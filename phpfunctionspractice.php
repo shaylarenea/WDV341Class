246b338268d74ec2cf1b0ff95c34d4af33882115
Shaylarenea <shayla.williamsr@gmail.com>
2023-12-04 21:24:09 -0600

    Initial Commit

---
+++ phpfunctionspractice.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>WDV341 Intro PHP</h1>
+    <h2>PHP Basics and examples</h2>
+    <?php
+    echo "<h3>This is an output from PHP on the server</h3>"
+    ?>
+</body>
+</html>  
